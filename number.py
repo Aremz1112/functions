1036f3b6ed01d084fc76ad70b89d7497317bb082
aremusemilore15-igtm <aremusemilore15@gmail.com>
2026-02-28 23:31:29 -0800

    added a python file

---
+++ number.py
@@ -0,0 +1,11 @@
+print ("Even numbers between 1 to 20:")
+for num in range (2, 21, 2):
+        print(num)
+
+print ("/nodd numbers between 1 to 20:")
+for num in range (1, 21, 2):
+    print (num)
+
+print ("\nMultiples of 10 between 1 and 100:")
+for num in range (10, 101, 10):
+    print(num)
